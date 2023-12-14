6f4c4f3acfb43c2da5469ffdd3580db7b3b005da
narnav <narnavg@gmail.com>
2023-12-14 18:33:45 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from icecream import ic
+
+students =["amir","mor","roee","yair"]
+student={"name":"itay","age":4,"email":"aaa@aaa.com"}
+
+print(students)
+ic(students)
+print(student)
+ic(student)
