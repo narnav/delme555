c99b660a8465f2a15f7746b02d37b4397fd818fd
narnav <narnavg@gmail.com>
2023-12-14 21:07:46 +0200

    aaa

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
-from icecream import ic
+import json
 
-students =["amir","mor","roee","yair"]
-student={"name":"itay","age":4,"email":"aaa@aaa.com"}
+cars = []
+my_file= "mydata.json"
 
-print(students)
-ic(students)
-print(student)
-ic(student)
+def print_all_cars():
+    for car in cars:
+        print(f'Your car: {car}')
+
+def save_cars_to_json():
+    with open("mydata.json", 'w') as file:
+        json.dump(cars, file)
+
+def load_cars_from_json():
+    global cars
+    try:
+        with open(my_file, 'r') as file:
+            cars = json.load(file)
+        print("data loaded")
+    except: 
+        print("data file not found")
+    
+
+def main():
+    load_cars_from_json()
+    while True:
+        print("a - add a car")
+        print("p - print all cars")
+        print("x - exit")
+        user_selection = input("Your selection: ")
+        
+        if user_selection == 'a':
+            cars.append('red')  # For the sake of example, adding 'red' as a placeholder
+        elif user_selection == 'x':
+            save_cars_to_json()
+            print(f'All cars saved to {my_file}')
+            exit()
+        elif user_selection == 'p':
+            print_all_cars()
+        else:
+            print("Select from menu only")
+
+if __name__ == '__main__':
+    main()
---
+++ draft.py
@@ -0,0 +1,6 @@
+def draw_board():
+    print('_|_|_')
+    print('_|_|_')
+    print(' | | ')
+
+draw_board(1)
